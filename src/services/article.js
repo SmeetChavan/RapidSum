b1f47bd3eb138f1a5ae6b81b4e217f99eb8486e1
SmeetChavan <smeet.br.chavan7@gmail.com>
2023-08-22 11:11:22 +0530

    API_KEY_HIDDEN

--- src/services/article.js
+++ src/services/article.js
@@ -1,7 +1,5 @@
 import { createApi , fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
-const rapidApiKey = import.meta.env.RAPID_API_ARTICLE_KEY;
-
 export const articleApi = createApi({
 
     reducerPath: 'articleApi',
@@ -11,7 +9,7 @@ export const articleApi = createApi({
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
 
         prepareHeaders: (headers) => {
-            headers.set('X-RapidAPI-Key' , rapidApiKey);
+            headers.set('X-RapidAPI-Key' , "[ENTER_YOUR_API_KEY_HERE]");
             headers.set('X-RapidAPI-Host' , 'article-extractor-and-summarizer.p.rapidapi.com');
 
             return headers;
